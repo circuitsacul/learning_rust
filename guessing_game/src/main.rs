224f05a352181c6eb74ef09aefa8ee81849b50d6
Circuit <circuitsacul@icloud.com>
2022-06-15 21:36:00 -0400

    guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let answer = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess = match guess
+            .trim()
+            .parse::<u32>() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please pass a positive number.");
+                    continue;
+                }
+            };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&answer) {
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            },
+            Ordering::Greater => println!("Too big."),
+            Ordering::Less => println!("Too small."),
+        }
+    }
+}
