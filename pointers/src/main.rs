7b415ea37bcc4b1af26ab6685f31380688b75981
Circuit <circuitsacul@icloud.com>
2022-06-18 20:33:58 -0400

    more smart pointers stuff

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,8 +1,12 @@
 mod cons_box;
 mod deref_drop;
+mod rc;
+mod refcell;
 
 
 fn main() {
     crate::cons_box::demo();
     crate::deref_drop::demo();
+    crate::rc::demo();
+    crate::refcell::demo();
 }
